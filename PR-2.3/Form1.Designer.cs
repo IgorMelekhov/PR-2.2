7484e4291653e15cb4a4533a68fc311f62af9937
IgorMelekhov <143938424+IgorMelekhov@users.noreply.github.com>
2024-04-27 10:01:01 +0300

    Add files via upload

---
+++ PR-2.3/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace PR_2._3
+{
+    partial class Dictionary
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonCalculation = new System.Windows.Forms.Button();
+            this.textBoxTotalNumberStudent = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxChanged = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1Count = new System.Windows.Forms.TextBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MaximumSize = new System.Drawing.Size(499, 24);
+            this.menuStrip1.MinimumSize = new System.Drawing.Size(499, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(499, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem,
+            this.cleanToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // cleanToolStripMenuItem
+            // 
+            this.cleanToolStripMenuItem.Name = "cleanToolStripMenuItem";
+            this.cleanToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.cleanToolStripMenuItem.Text = "Очистить ";
+            this.cleanToolStripMenuItem.Click += new System.EventHandler(this.cleanToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.AboutToolStripMenuItem.Text = "О программе";
+            this.AboutToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(330, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Программа хранит данные о числе студентов в разных группах\r\n";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.buttonClear);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.buttonCalculation);
+            this.groupBox1.Controls.Add(this.textBoxTotalNumberStudent);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.textBoxCount);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.buttonDelete);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxChanged);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1Count);
+            this.groupBox1.Controls.Add(this.buttonChange);
+            this.groupBox1.Location = new System.Drawing.Point(12, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(475, 275);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label7.Location = new System.Drawing.Point(215, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Общее кол-во студентов";
+            // 
+            // buttonCalculation
+            // 
+            this.buttonCalculation.Location = new System.Drawing.Point(156, 213);
+            this.buttonCalculation.Name = "buttonCalculation";
+            this.buttonCalculation.Size = new System.Drawing.Size(73, 21);
+            this.buttonCalculation.TabIndex = 19;
+            this.buttonCalculation.Text = "Посчитать ";
+            this.buttonCalculation.UseVisualStyleBackColor = true;
+            this.buttonCalculation.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxTotalNumberStudent
+            // 
+            this.textBoxTotalNumberStudent.Location = new System.Drawing.Point(230, 214);
+            this.textBoxTotalNumberStudent.Name = "textBoxTotalNumberStudent";
+            this.textBoxTotalNumberStudent.ReadOnly = true;
+            this.textBoxTotalNumberStudent.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTotalNumberStudent.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label6.Location = new System.Drawing.Point(269, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Кол-во студентов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Location = new System.Drawing.Point(287, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Название";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(269, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 14;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(147, 122);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(112, 21);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "Добавить группу";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(265, 146);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 12;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(265, 106);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 11;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(295, 248);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(174, 21);
+            this.buttonDelete.TabIndex = 10;
+            this.buttonDelete.Text = "Удалить выбранную группу";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(178, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Изменить кол-во студентов на\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(246, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Кол-во студентов";
+            // 
+            // textBoxChanged
+            // 
+            this.textBoxChanged.Location = new System.Drawing.Point(347, 45);
+            this.textBoxChanged.Name = "textBoxChanged";
+            this.textBoxChanged.Size = new System.Drawing.Size(100, 20);
+            this.textBoxChanged.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2-ИСП",
+            "2-ТЭО"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1Count
+            // 
+            this.textBox1Count.Location = new System.Drawing.Point(347, 19);
+            this.textBox1Count.Name = "textBox1Count";
+            this.textBox1Count.ReadOnly = true;
+            this.textBox1Count.Size = new System.Drawing.Size(100, 20);
+            this.textBox1Count.TabIndex = 4;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(362, 71);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(73, 21);
+            this.buttonChange.TabIndex = 6;
+            this.buttonChange.Text = "Изменить \r\n";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(336, 214);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(73, 21);
+            this.buttonClear.TabIndex = 21;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Dictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximumSize = new System.Drawing.Size(515, 390);
+            this.MinimumSize = new System.Drawing.Size(515, 390);
+            this.Name = "Dictionary";
+            this.Text = "Словарь";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1Count;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.TextBox textBoxChanged;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonCalculation;
+        private System.Windows.Forms.TextBox textBoxTotalNumberStudent;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem cleanToolStripMenuItem;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ PR-2.3/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PR_2._3
+{
+    public partial class Dictionary : Form
+    {
+        public Dictionary()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            textBoxChanged.Validating += textBoxChanged_Validating;
+            textBoxCount.Validating += textBoxCount_Validating;
+        }
+        Dictionary<string, int> group = new Dictionary<string, int>()
+        {
+            ["2-ИСП"] = 25,
+            ["2-ТЭО"] = 24,
+        };
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedState= comboBox1.SelectedItem.ToString();
+            int counter = 0;
+            if (group.TryGetValue(selectedState,out counter)) textBox1Count.Text = counter.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectGroup = null;
+            if ( comboBox1.SelectedItem != null)
+            {
+                try
+                {
+                    selectGroup = comboBox1.SelectedItem.ToString();
+                    this.group[selectGroup] = Convert.ToInt32(textBoxChanged.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Введены неверные значения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBoxChanged.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы не выбрали группу", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxChanged.Clear();
+            }
+            textBoxChanged.Clear();
+            textBox1Count.Clear();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            if (!string.IsNullOrEmpty(name))
+            {
+                try
+                {
+
+                    int count = Convert.ToInt16(textBoxCount.Text);
+                    group.Add(name, count);
+                    List<string> list = new List<string>(group.Keys);
+                    comboBox1.DataSource = list;
+                }
+                catch
+                {
+                    MessageBox.Show("Введены неверные значения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBoxName.Clear();
+                    textBoxCount.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели название группы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxName.Clear();
+                textBoxCount.Clear();
+            }
+            textBoxName.Clear() ;
+            textBoxCount.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string name = comboBox1.SelectedItem.ToString();
+                group.Remove(name);
+                List<string> list = new List<string>(group.Keys);
+                comboBox1.DataSource = list;
+            }
+            else
+            {
+                MessageBox.Show("Вы не выбрали группу", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxChanged.Clear();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int sum = group.Values.Sum();
+            textBoxTotalNumberStudent.Text = sum.ToString();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 newF = new Form2();
+            newF.Show();
+        }
+        private void textBoxChanged_Validating(object sender, CancelEventArgs e)
+        {
+            int inputNumber;
+            if (!String.IsNullOrEmpty(textBoxChanged.Text))
+            {
+                if (!int.TryParse(textBoxChanged.Text, out inputNumber))
+                {
+                    errorProvider1.SetError(textBoxChanged, "В поле должно быть введено целое число!");
+                }
+                else errorProvider1.Clear();
+            }
+            else errorProvider1.Clear();
+        }
+        private void textBoxCount_Validating(object sender, CancelEventArgs e)
+        {
+            int inputNumber;
+            if (!String.IsNullOrEmpty(textBoxCount.Text))
+            {
+                if (!int.TryParse(textBoxCount.Text, out inputNumber))
+                {
+                    errorProvider1.SetError(textBoxCount, "В поле должно быть введено целое число!");
+                }
+                else errorProvider1.Clear();
+            }
+            else errorProvider1.Clear();
+        }
+
+        private void cleanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxCount.Clear();
+            textBoxName.Clear();
+            textBoxChanged.Clear();
+            textBoxTotalNumberStudent.Clear();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxCount.Clear();
+            textBoxName.Clear();
+            textBoxChanged.Clear();
+            textBoxTotalNumberStudent.Clear();
+        }
+    }
+}
